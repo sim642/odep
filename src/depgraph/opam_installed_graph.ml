a19b35a012f65dd2f03d667ee7d7137691af808d
Simmo Saan <simmo.saan@gmail.com>
2022-11-06 17:20:00 +0200

    Implement opam-installed --depends argument

--- src/depgraph/opam_installed_graph.ml
+++ src/depgraph/opam_installed_graph.ml
@@ -7,10 +7,52 @@ module Ofml = OpamFormula
 
 module GOper = Graph.Oper.P (G)
 
-let g_of_installed ~tred_packages ?depends ?rdepends () =
-  ignore (depends);
-  ignore (rdepends);
+let g_of_depends ~st ~env depends =
+  let {u_installed; u_depends; u_depopts; _} =
+    OpamSwitchState.universe st ~requested:Opkg.Name.Set.empty Query
+  in
 
+  let fold_all_depends f acc package =
+    let deps = Opkg.Map.find package u_depends |> OpamPackageVar.filter_depends_formula ~env in
+    let depopts = Opkg.Map.find package u_depopts |> OpamPackageVar.filter_depends_formula ~env in
+
+    let acc = Ofml.fold_left (fun acc (name, _) ->
+        f acc name
+      ) acc deps
+    in
+    Ofml.fold_left (fun acc (name, _) ->
+       f acc name
+      ) acc depopts
+  in
+
+  let fold_package package g =
+    let pkg: V.t = OpamPackage (Opkg.Name.to_string package.name) in
+    let g = G.add_vertex g pkg in
+    fold_all_depends (fun g name ->
+        G.add_edge g pkg (OpamPackage (Opkg.Name.to_string name))
+      ) g package
+  in
+
+  match depends with
+  | None ->
+    Opkg.Set.fold fold_package u_installed G.empty
+  | Some depends ->
+    let rec fold_deps_package g visited package =
+      if Opkg.Set.mem package visited || not (Opkg.Set.mem package u_installed) then
+        g
+      else (
+        let visited = Opkg.Set.add package visited in
+        let g = fold_package package g in
+        fold_all_depends (fun g name ->
+            fold_deps_name g visited name
+          ) g package
+      )
+    and fold_deps_name g visited name =
+      fold_deps_package g visited (OpamSwitchState.get_package st name)
+    in
+    fold_deps_name G.empty Opkg.Set.empty (Opkg.Name.of_string depends)
+
+let g_of_installed ~tred_packages ?depends ?rdepends () =
   let root = OpamStateConfig.opamroot () in
   OpamFormatConfig.init ();
   ignore (OpamStateConfig.load_defaults root);
@@ -21,28 +63,8 @@ let g_of_installed ~tred_packages ?depends ?rdepends () =
   OpamSwitchState.with_ `Lock_none gt @@ fun st ->
   let env = OpamPackageVar.resolve st in
 
-  let {u_installed; u_depends; u_depopts; _} =
-    OpamSwitchState.universe st ~requested:Opkg.Name.Set.empty Query
-  in
-
-  let g = Opkg.Set.fold (fun package g ->
-      let pkg: V.t = OpamPackage (Opkg.Name.to_string package.name) in
-      let g = G.add_vertex g pkg in
-
-      let deps = Opkg.Map.find package u_depends |> OpamPackageVar.filter_depends_formula ~env in
-      let depopts = Opkg.Map.find package u_depopts |> OpamPackageVar.filter_depends_formula ~env in
-
-      let g = Ofml.fold_left (fun g (name, _) ->
-          G.add_edge g pkg (OpamPackage (Opkg.Name.to_string name))
-        ) g deps
-      in
-      let g = Ofml.fold_left (fun g (name, _) ->
-          G.add_edge g pkg (OpamPackage (Opkg.Name.to_string name))
-        ) g depopts
-      in
-      g
-    ) u_installed G.empty
-  in
+  let g = g_of_depends ~st ~env depends in
+  ignore (rdepends);
 
   if tred_packages then
     GOper.transitive_reduction g
