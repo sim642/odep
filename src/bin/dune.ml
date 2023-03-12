8bf81ea32cb2cd1eb1817a3448c8eca6422e9364
Simmo Saan <simmo.saan@gmail.com>
2023-03-12 12:10:20 +0200

    Add --with-pps flag to odep dune

--- src/bin/dune.ml
+++ src/bin/dune.ml
@@ -1,9 +1,10 @@
 open Bos
 
 open Depgraph
+open Common
 open Std.Result_syntax
 
-let run path (`Type type_) (`Tred_modules tred_modules) (`Tred_libraries tred_libraries) =
+let run path (`Type type_) (`Tred_modules tred_modules) (`Tred_libraries tred_libraries) (`With_pps with_pps) =
   let f = Fpath.v path in
   let* dir =
     let+ is_dir = OS.Dir.exists f in
@@ -13,16 +14,20 @@ let run path (`Type type_) (`Tred_modules tred_modules) (`Tred_libraries tred_li
       Fpath.parent f
   in
   let+ s = OS.Dir.with_current dir (fun () ->
-      let dune_describe' = Cmd.(v "dune" % "describe" % "workspace" % "--with-deps") in
-      let dune_describe = Cmd.(dune_describe' % "--with-pps") in
-      match OS.Cmd.(run_out dune_describe |> out_string) with
-      | Ok (s, (_, `Exited 0)) ->
-        Ok s
-      | Ok (_, (_, `Exited _)) -> (* maybe dune < 3.7.0, which doesn't have --with-pps *)
-        OS.Cmd.(run_out dune_describe' |> out_string |> success)
-      | r ->
-        OS.Cmd.success r
-    ) ()
+      let dune_describe = Cmd.(v "dune" % "describe" % "workspace" % "--with-deps") in
+      match with_pps with
+      | None ->
+        begin match OS.Cmd.(run_out Cmd.(dune_describe % "--with-pps") |> out_string) with
+          | Ok (s, (_, `Exited 0)) ->
+            Ok s
+          | Ok (_, (_, `Exited _)) -> (* maybe dune < 3.7.0, which doesn't have --with-pps *)
+            OS.Cmd.(run_out dune_describe |> out_string |> success)
+          | r ->
+            OS.Cmd.success r
+        end
+      | Some with_pps ->
+        OS.Cmd.(run_out Cmd.(dune_describe %% on with_pps (v "--with-pps")) |> out_string |> success)
+      ) ()
     |> Result.join
   in
   Dune_describe_graph.g_of_string ~tred_modules ~tred_libraries s
@@ -34,8 +39,12 @@ let path =
   let doc = "Dune project location." in
   Arg.(value & pos 0 file "." & info [] ~docv:"PATH" ~doc)
 
+let with_pps =
+  let doc = "Include compile-time ppx dependencies." in
+  Arg.(term_map (fun b -> `With_pps b) & value & opt (some bool) None & info ["with-pps"] ~docv:"BOOL" ~doc)
+
 let term =
-  Term.cli_parse_result Term.(const run $ path $ Common.type_ $ Common.tred_modules $ Common.tred_libraries)
+  Term.cli_parse_result Term.(const run $ path $ Common.type_ $ Common.tred_modules $ Common.tred_libraries $ with_pps)
 
 let cmd =
   let doc = "Generate dependency graph from dune project." in
